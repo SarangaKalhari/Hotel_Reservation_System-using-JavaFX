1f0840d1b5585a124c190d4dcb94809be2ed8f76
SarangaKalhari <kalharisaranga9@gmail.com>
2025-10-07 16:44:55 +0530

    Clear function

--- src/main/java/controller/Room_Info.java
+++ src/main/java/controller/Room_Info.java
@@ -63,6 +63,10 @@ public class Room_Info implements Initializable {
 
     @FXML
     void btnClearOnAction(ActionEvent event) {
+        txtRoomId.clear();
+        txtType.clear();
+        txtDesc.clear();
+        txtPrice.clear();
 
     }
 
