9f2ac7ff21324365e67f7480157231d8a9493979
SarangaKalhari <kalharisaranga9@gmail.com>
2025-10-07 14:42:06 +0530

    Add Customer Info UI

--- src/main/java/controller/Customer_Info.java
+++ src/main/java/controller/Customer_Info.java
@@ -1,5 +1,69 @@
 package controller;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+
 public class Customer_Info {
+
+    @FXML
+    private TableColumn<?, ?> colCustAge;
+
+    @FXML
+    private TableColumn<?, ?> colCustCity;
+
+    @FXML
+    private TableColumn<?, ?> colCustID;
+
+    @FXML
+    private TableColumn<?, ?> colCustName;
+
+    @FXML
+    private TableColumn<?, ?> colCustPhoneNo;
+
+    @FXML
+    private TableView<?> tblRoomInfo;
+
+    @FXML
+    private TextField txtAge;
+
+    @FXML
+    private TextField txtCity;
+
+    @FXML
+    private TextField txtCustID;
+
+    @FXML
+    private TextField txtName;
+
+    @FXML
+    private TextField txtPhoneNumber;
+
+    @FXML
+    void btnAddOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnClearOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnDeleteOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnReloadOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnUpdateOnAction(ActionEvent event) {
+
+    }
 
 }
--- src/main/java/model/dto/CustomerInfoDTO.java
+++ src/main/java/model/dto/CustomerInfoDTO.java
@@ -1,4 +1,14 @@
 package model.dto;
 
 public class CustomerInfoDTO {
+
+    private String id;
+
+    private String name;
+
+    private String phoneNo;
+
+    private String city;
+
+    private int age;
 }
